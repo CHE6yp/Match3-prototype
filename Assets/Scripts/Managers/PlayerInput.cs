772a621afbe8dd96d06247677678ff7c6c1937ef
I.Kusov <i.kusov@tendertech.ru>
2020-02-10 18:08:31 +0300

    Swiping

--- Assets/Scripts/Managers/PlayerInput.cs
+++ Assets/Scripts/Managers/PlayerInput.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+
+    public Vector2 startPos;
+    public Vector2 direction;
+    public bool directionChosen;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +28,47 @@ public class PlayerInput : MonoBehaviour
             Debug.DrawRay(ray.origin, ray.direction*100, Color.yellow);
         }
 
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
 
+            // Handle finger movements based on touch phase.
+            switch (touch.phase)
+            {
+                // Record initial touch position.
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    directionChosen = false;
+                    break;
+
+                // Determine direction by comparing the current touch position with the initial one.
+                case TouchPhase.Moved:
+                    direction = touch.position - startPos;
+                    break;
+
+                // Report that a direction has been chosen when the finger is lifted.
+                case TouchPhase.Ended:
+                    directionChosen = true;
+
+                    Debug.Log(direction);
+                    if (direction.magnitude >= 100){
+
+                        GameObject.Find("debug").GetComponent<UnityEngine.UI.Text>().text = direction.normalized.ToString();
+                        if (Mathf.Abs(direction.normalized.x)>Mathf.Abs(direction.normalized.y))
+                        {
+                            //if x<0 left, x>0 right
+                        }
+                        if (Mathf.Abs(direction.normalized.x)<Mathf.Abs(direction.normalized.y))
+                        {
+                            //if y<0 down, y>0 up
+                        }
+                    }
+                    break;
+            }
+        }
+        if (directionChosen)
+        {
+            // Something that uses the chosen direction...
+        }
     }
 }
