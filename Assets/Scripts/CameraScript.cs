16c6681b0ceb36d28572122190e1e273bf4a552c
CHE6yp <che6yp@gmail.com>
2020-04-05 18:18:47 +0300

    Background tweaks

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,6 +11,8 @@ public class CameraScript : MonoBehaviour
     void Start()
     {
         Camera.main.transform.position += new Vector3((GameManager.instance.field.width - 1) * (GameManager.instance.itemInterval / 2), 0);
+        background.transform.position  += new Vector3((GameManager.instance.field.width - 1) * (GameManager.instance.itemInterval / 2), 0);
+
         Camera.main.orthographicSize = (GameManager.instance.field.width * GameManager.instance.itemInterval + margin) * Screen.height / Screen.width * 0.5f;
     }
 }
