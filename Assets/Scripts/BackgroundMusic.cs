3f883e6fc0ec5e0a20c5afd49783cc33c1b0639a
CHE6yp <che6yp@gmail.com>
2020-04-05 15:48:38 +0300

    Background Music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public List<AudioClip> loops;
+    public AudioSource audioSource;
+    
+    void Start()
+    {
+        audioSource.clip = loops[Random.Range(0, loops.Count)];
+        audioSource.Play();
+    }
+}
